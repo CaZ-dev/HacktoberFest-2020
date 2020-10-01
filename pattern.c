6661aeaef94ab246cd531c8124f4277d605a7a8d
prasanna1432 <71954483+prasanna1432@users.noreply.github.com>
2020-10-01 18:24:31 +0530

    Create pattern.c

---
+++ pattern.c
@@ -0,0 +1,17 @@
+int main()
+{
+  int n, c, k;
+
+  printf("Enter number of rows\n");
+  scanf("%d", &n);
+
+  for (c = 1; c <= n; c++)
+  {
+    for(k = 1; k <= c; k++)
+      printf("*");
+
+    printf("\n");
+  }
+
+  return 0;
+}
